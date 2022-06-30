901335df067bc8e7e7c4b3ee4de6cb8e6f856678
Santiago Xamena <xamenasantiago@hotmail.com>
2022-06-29 23:20:18 -0300

    trabaje 5 ejercicios de js basicos

---
+++ js/2-practica.js
@@ -0,0 +1,18 @@
+//mensaje de venta emergente
+alert('desde una ventana emergente');
+
+//mostrar un msj en la pantalla o documento
+document.write('<h1> hello world</h1>');
+
+//sumemos dos valores
+document.write(3+5);
+
+
+//Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+var nombre = prompt("santiago xamena");
+document.write("Hola "+nombre);
+
+//Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+var n1 = prompt("10");
+var n2 = prompt("12");
+document.write("La suma es: "+ ( parseInt(n1)+ parseInt(n2) ) );
