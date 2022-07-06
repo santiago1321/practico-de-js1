73424d19bce063a78235087f5309b4400d1f5dce
Santiago Xamena <xamenasantiago@hotmail.com>
2022-07-06 19:29:29 -0300

    termine ejercicio 1 al 5

--- js/2-practica.js
+++ js/2-practica.js
@@ -2,7 +2,7 @@
 alert('desde una ventana emergente');
 
 //mostrar un msj en la pantalla o documento
-document.write('<h1> hello world</h1>');
+document.write('<br><h1> hello world</h1>');
 
 //sumemos dos valores
 document.write(3+5);
@@ -10,9 +10,9 @@ document.write(3+5);
 
 //Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 var nombre = prompt("santiago xamena");
-document.write("Hola "+nombre);
+document.write("<br>Hola"+nombre);
 
 //Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
-var n1 = prompt("10");
-var n2 = prompt("12");
-document.write("La suma es: "+ ( parseInt(n1)+ parseInt(n2) ) );
+let= n1 = prompt("10");
+let= n2 = prompt("12");
+document.write("<br>La suma es:" + (parseInt(n1) + parseInt(n2)));git 
