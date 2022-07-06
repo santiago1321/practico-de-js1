f965dd024cb6404272c225acaf23ded92ebf12a5
Santiago Xamena <xamenasantiago@hotmail.com>
2022-07-06 19:33:10 -0300

    agrego ejercicio 9

---
+++ js/ejercicio9 -tp1.js
@@ -0,0 +1,33 @@
+let frase = prompt('ingrese una frase de 10 caracteres');
+consola.log(frase);
+consola.log(frase.length)
+console.log(frase.charAt(0));
+console.log(frase. charAt(frase.length -1))
+console.log(frase.toLowerCase());
+
+//actualizar la variable frase
+frase = frase.toLowerCase();
+//hola mundo
+
+if(frase.charAt(0)=== 'a' || frase.charAt(0)=== 'e' || frase.charAt(0)=== 'i' || frase.charAt(0)=== 'o' || frase.charAt(0) === 'u'){
+    document.write(frase.charAt(0));
+}
+
+if(frase.charAt(1)=== 'a' || frase.charAt(1)=== 'e' || frase.charAt(1)=== 'i' || frase.charAt(1)=== 'o' || frase.charAt(1) === 'u'){
+    document.write(frase.charAt(1));
+}
+if(frase.charAt(2)=== 'a' || frase.charAt(2)=== 'e' || frase.charAt(2)=== 'i' || frase.charAt(2)=== 'o' || frase.charAt(2) === 'u'){
+    document.write(frase.charAt(2));
+}
+
+if(frase.charAt(3)=== 'a' || frase.charAt(3)=== 'e' || frase.charAt(3)=== 'i' || frase.charAt(3)=== 'o' || frase.charAt(3) === 'u'){
+    document.write(frase.charAt(3));
+}
+
+if(frase.charAt(5)=== 'a' || frase.charAt(5)=== 'e' || frase.charAt(5)=== 'i' || frase.charAt(5)=== 'o' || frase.charAt(5) === 'u'){
+    document.write(frase.charAt(5));
+}
+
+if(frase.charAt(6)=== 'a' || frase.charAt(6)=== 'e' || frase.charAt(6)=== 'i' || frase.charAt(6)=== 'o' || frase.charAt(6) === 'u'){
+    document.write(frase.charAt(6));
+}
